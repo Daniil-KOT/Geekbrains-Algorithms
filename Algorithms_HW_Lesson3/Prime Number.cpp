0785fc891ada9e93f0e2811b9fbc9a52baecd2ec
Daniil <d.dulov98@gmail.com>
2021-05-16 19:27:43 +0400

    added HW for lesson 3

---
+++ Algorithms_HW_Lesson3/Prime Number.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x;
+    bool prime = true;
+    cout << "Enter the number: ";
+    cin >> x;
+
+    for (int i = 2; i < x; i++)
+    {
+        if (x % i == 0)
+        {
+            prime = false;
+            break;
+        }
+    }
+    if (prime == true)
+        cout << "The number is prime." << endl;
+    else
+        cout << "The number is not prime." << endl;
+}
+
